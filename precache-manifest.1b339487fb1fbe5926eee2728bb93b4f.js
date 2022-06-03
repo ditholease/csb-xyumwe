8acaf7974215ccc1ea2e7c2077e9d6b84d4d6b82
Code Sandbox <hello@codesandbox.io>
2022-06-03 05:38:27 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1b339487fb1fbe5926eee2728bb93b4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c5a88ca20baba6895ffdd3cc12fff0",
+    "url": "https://ditholease.github.io/csb-xyumwe/index.html"
+  },
+  {
+    "revision": "9e083ec8a4fb6b13a2b6",
+    "url": "https://ditholease.github.io/csb-xyumwe/static/css/2.4173a4ba.chunk.css"
+  },
+  {
+    "revision": "7fdcb95a08f64cde137a",
+    "url": "https://ditholease.github.io/csb-xyumwe/static/css/main.45d4fbeb.chunk.css"
+  },
+  {
+    "revision": "9e083ec8a4fb6b13a2b6",
+    "url": "https://ditholease.github.io/csb-xyumwe/static/js/2.ccf62389.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "https://ditholease.github.io/csb-xyumwe/static/js/2.ccf62389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdcb95a08f64cde137a",
+    "url": "https://ditholease.github.io/csb-xyumwe/static/js/main.f1853fe0.chunk.js"
+  },
+  {
+    "revision": "14bcb28f51c2361e67fd",
+    "url": "https://ditholease.github.io/csb-xyumwe/static/js/runtime-main.352b0a32.js"
+  }
+]);
